d37ac3a779e9f3964f55a93a73e8c2b59abb43e8
besoftvn <besoft.provider@gmail.com>
2023-10-07 17:29:19 +0700

    Updates

---
+++ precache-manifest.6126f6da6075aafdfd2935d05625d3f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a71436422def743075a947754a31cf25",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d29fb5073590b575c92a",
+    "url": "./static/css/main.66dbc06b.chunk.css"
+  },
+  {
+    "revision": "71ad31422afd20101713",
+    "url": "./static/js/2.738e4cc7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.738e4cc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d29fb5073590b575c92a",
+    "url": "./static/js/main.3bd3fb49.chunk.js"
+  },
+  {
+    "revision": "f3518aa0e4ad7675b967",
+    "url": "./static/js/runtime-main.155ba53a.js"
+  }
+]);
